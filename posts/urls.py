f818c4623a3e865c3243c6c6e3232b2ab0435179
Cesar Bernal <cesarbworkmail@gmail.com>
2019-03-26 17:49:41 +0000

    showing projects and edit function ready

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
-from posts.views import project_create
+from posts.views import project_page, project_create, edit_project
 from accounts import url_reset
 
 urlpatterns = [
-    url(r'^$', project_create, name="project_create"),
+    url(r'^$', project_page, name="project_page"),
+    url(r'^create/$', project_create, name="project_create"),
+    url(r'^(?P<pk>\d+)/edit/$', edit_project, name='edit_project')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import auth, messages
+from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from posts.models import project
@@ -7,7 +8,15 @@ from posts.forms import createProject
 
 
 # Create your views here.
-
+def project_page(request):
+    """ Create a view that will return a list
+    of posts that are published prior to now
+    and render them to the blogpost.html template
+    """
+    projects = project.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request,"posts.html", {'projects': projects})
+    
+    
 @login_required
 def project_create(request):
     """project form"""
@@ -21,9 +30,26 @@ def project_create(request):
             instance = newProject_form.save(commit=False)
             instance.author = request.user
             instance.save()
-            return redirect(project_create)
+            return redirect(project_page)
 
     else:
         newProject_form = createProject()
-    return render(request, 'posts.html', {'newProject_form': newProject_form})
+    return render(request, 'create_posts.html', {'newProject_form': newProject_form})
     
+def edit_project(request, pk=None):
+    """create a view that allows us to create 
+    or edit a post depending if the post ID
+    is null or not
+    """
+    
+    post = get_object_or_404(project, pk=pk) if pk else None
+    if request.method=='POST':
+        form = createProject(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.author = request.user
+            instance.save()
+            return redirect(project_page)
+    else:
+        form = createProject(instance=post)
+    return render(request, 'posts_form.html', {'form': form})
