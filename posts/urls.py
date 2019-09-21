30104e732bf351ba3cd72dc2020d26c6db1b007e
cesar bernal <cesarbworkmail@gmail.com>
2019-09-21 14:21:38 +0200

    changes to code delete not working

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
-from posts.views import project_page, project_create, edit_project
+from posts.views import project_page, project_create, edit_project, delete_post
 from accounts import url_reset
 
 urlpatterns = [
     url(r'^$', project_page, name="project_page"),
     url(r'^create/$', project_create, name="project_create"),
-    url(r'^(?P<pk>\d+)/edit/$', edit_project, name='edit_project')
+    url(r'^(?P<pk>\d+)/edit/$', edit_project, name='edit_project'),
+    url(r'^(?P<pk>\d+)/delete/$', delete_post, name='delete_post')
 ]
--- posts/views.py
+++ posts/views.py
@@ -15,12 +15,12 @@ def project_page(request):
     """
     projects = project.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request,"posts.html", {'projects': projects})
-    
-    
+
+
 @login_required
 def project_create(request):
     """project form"""
-   
+
     if request.method == "POST":
         newProject_form = createProject(request.POST)
 
@@ -35,13 +35,13 @@ def project_create(request):
     else:
         newProject_form = createProject()
     return render(request, 'create_posts.html', {'newProject_form': newProject_form})
-    
+
 def edit_project(request, pk=None):
-    """create a view that allows us to create 
+    """create a view that allows us to create
     or edit a post depending if the post ID
     is null or not
     """
-    
+
     post = get_object_or_404(project, pk=pk) if pk else None
     if request.method=='POST':
         form = createProject(request.POST, request.FILES, instance=post)
@@ -53,3 +53,10 @@ def edit_project(request, pk=None):
     else:
         form = createProject(instance=post)
     return render(request, 'posts_form.html', {'form': form})
+
+def delete_post(request, pk):
+    post = get_object_or_404(project, pk=pk)
+    if request.method=='POST':
+        post.delete()
+        return redirect(project_page)
+    return render(request,"posts.html")
