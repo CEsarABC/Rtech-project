15d7799fc0828f340ccdbcd7455511d611d655f7
cesar bernal <cesarbworkmail@gmail.com>
2019-11-26 13:05:24 +0100

    small changes

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from posts.models import project
+from posts.models import project, servicesUpdate
 
 # Register your models here.
 admin.site.register(project)
+admin.site.register(servicesUpdate)
